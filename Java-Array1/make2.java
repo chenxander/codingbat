8662da77247edc19cccc74e26ab4573d6411a256
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-23 12:12:39 -0500

    Create make2.java

---
+++ Java-Array1/make2.java
@@ -0,0 +1,14 @@
+public int[] make2(int[] a, int[] b) {
+  int[] output = new int[2];
+  if (a.length >= 2) {
+    output[0] = a[0];
+    output[1] = a[1];
+  } else if (a.length == 1) {
+    output[0] = a[0];
+    output[1] = b[0];
+  } else {
+    output[0] = b[0];
+    output[1] = b[1];
+  }
+  return output;
+}
