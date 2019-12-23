4ffa3e57a78e5aa707a8ed618cdcaa6e0360f62b
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-23 11:01:13 -0500

    Create twoChar.java

---
+++ Java-String1/twoChar.java
@@ -0,0 +1,7 @@
+public String twoChar(String str, int index) {
+  if (index > str.length()-1 || index+1 > str.length()-1
+  || index < 0) {
+    return str.substring(0,2);
+  }
+  return str.substring(index,index+2);
+}
