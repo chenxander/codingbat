999ad8294f6e38b9b597b53e24a1c678cc4a8a63
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-25 10:14:14 -0500

    Create has77.java

---
+++ Java-Array2/has77.java
@@ -0,0 +1,18 @@
+public boolean has77(int[] nums) {
+  if (nums.length < 2) {
+    return false;
+  }
+  for (int i = 0; i < nums.length-1; i++) {
+    if (nums[i] == 7 && nums[i+1] == 7) {
+      return true;
+    }
+  }
+  if (nums.length>2) {
+    for (int i = 0; i < nums.length-2; i++) {
+      if (nums[i] == 7 && nums[i+2] == 7) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
