b55a6d40db14ef81c4f1bc93f606ab0041093203
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-25 11:22:29 -0500

    Create notAlone.java

---
+++ Java-Array2/notAlone.java
@@ -0,0 +1,11 @@
+public int[] notAlone(int[] nums, int val) {
+  if (nums.length<3) {
+    return nums;
+  }
+  for (int i = 1; i < nums.length-1; i++) {
+    if (nums[i] == val && nums[i-1] != nums[i] && nums[i+1] != nums[i]) {
+      nums[i] = Math.max(nums[i-1],nums[i+1]);
+    }
+  }
+  return nums;
+}
