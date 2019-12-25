ea18759e992ea714cb7b4f9103b6faed828b59ef
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-25 09:49:21 -0500

    Create sum28.java

---
+++ Java-Array2/sum28.java
@@ -0,0 +1,12 @@
+public boolean sum28(int[] nums) {
+  int count = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      count++;
+    }
+  }
+  if (count == 4) {
+    return true;
+  }
+  return false;
+}
