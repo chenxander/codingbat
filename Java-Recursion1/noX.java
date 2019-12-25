fd8d7cba0f73e9c290e8a4ab0106cd14d86c3402
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-25 14:18:29 -0500

    Create noX.java

---
+++ Java-Recursion1/noX.java
@@ -0,0 +1,8 @@
+public String noX(String str) {
+  if (str.length() == 0) return "";
+  if (str.charAt(0) == 'x') {
+    return noX(str.substring(1,str.length()));
+  } else {
+    return str.charAt(0) + noX(str.substring(1,str.length()));
+  }
+}
