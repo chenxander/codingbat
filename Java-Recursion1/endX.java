b7a166ef4b552f12ebd979e2a7417648e56c5fb6
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-25 15:07:18 -0500

    Create endX.java

---
+++ Java-Recursion1/endX.java
@@ -0,0 +1,7 @@
+public String endX(String str) {
+  if (str.length() == 0) return "";
+  if (str.charAt(0) == 'x') {
+    return endX(str.substring(1,str.length())) + 'x';
+  }
+  return str.charAt(0) + endX(str.substring(1,str.length()));
+}
