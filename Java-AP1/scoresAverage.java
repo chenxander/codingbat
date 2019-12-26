59137384f047ab55daf311b7a8611bc69482251b
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-26 10:19:34 -0500

    Create scoresAverage.java

---
+++ Java-AP1/scoresAverage.java
@@ -0,0 +1,12 @@
+public int scoresAverage(int[] scores) {
+  return Math.max(average(scores, 0, scores.length/2),
+  average(scores,scores.length/2,scores.length));
+}
+
+int average (int[] scores, int start, int end) {
+  int sum = 0;
+  for (int i = start; i < end; i++) {
+    sum += scores[i];
+  }
+  return sum /= (end - start);
+}
