40d43e45f6fcebb921ddd4f48e2b82701697031c
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-28 16:12:39 -0500

    Update makeBricks.java

--- Java-Logic2/makeBricks.java
+++ Java-Logic2/makeBricks.java
@@ -1,1 +1,10 @@
-
+public boolean makeBricks(int small, int big, int goal) {
+  for (int i = 0; i <= big; i++) {
+    for (int j = 0; j <= small; j++) {
+      if (i*5 + j == goal) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
