093e0e72999730afae0bcfaab97d712bfe97e8d9
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-30 16:26:52 -0500

    Create sameEnds.java

---
+++ Java-String3/sameEnds.java
@@ -0,0 +1,9 @@
+public String sameEnds(String string) {
+  for (int i = string.length()/2; i > 0; i--) {
+    if (string.substring(0,i)
+    .equals(string.substring(string.length()-i,string.length()))) {
+      return string.substring(0,i);
+    }
+  }
+  return "";
+}
