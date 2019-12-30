7b7a41adfd997bbf730d941b8eeda672f5f5448c
Alex <58018549+chenxander@users.noreply.github.com>
2019-12-30 14:10:46 -0500

    Create gHappy.java

---
+++ Java-String3/gHappy.java
@@ -0,0 +1,11 @@
+public boolean gHappy(String str) {
+  if (str.length() == 0) return true;
+  if (str.length() == 1 && !str.equals("g")) return false;
+  else if (str.length()==1) return false;
+  if (str.charAt(0) == 'g' && str.charAt(1) != 'g') return false;
+  if (str.charAt(str.length()-1) == 'g' && str.charAt(str.length()-2) != 'g') return false;
+  for (int i = 1; i < str.length()-1; i++) {
+    if (str.charAt(i) == 'g' && !(str.charAt(i-1) == 'g' || str.charAt(i+1) == 'g')) return false;
+  }
+  return true;
+}
